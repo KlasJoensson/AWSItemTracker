3ab80005dbc243a00295e2b9744e09d883366c2e
KlasJoensson <klas.joensson@gmail.com>
2020-11-25 09:49:41 +0100

    Init project

---
+++ AWSItemTracker/src/main/java/com/example/AWSItemTracker/AwsItemTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.AWSItemTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsItemTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsItemTrackerApplication.class, args);
+	}
+
+}
